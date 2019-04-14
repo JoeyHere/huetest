43cdc44e250fc37bb88edbb1e79ab497a9ff9ec0
JoeyHere <josephodell@outlook.com>
2019-04-14 23:24:29 +0100

    heroku

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,6 +27,7 @@ export default class Navbar extends Component {
             to="/create"
           />
           <Image
+            className={"hueLogo"}
             rounded
             style={{ margin: "10px", width: "153px", height: "27px" }}
             src="https://i.ibb.co/34DXmpm/Hueman-logo.png"
