6e227de2b075ddef4ee83a6e7bb9b0ca11f4d0e9
Joseph O'Dell <45050545+JoeyHere@users.noreply.github.com>
2020-02-11 11:29:19 +0000

    Update API.js

--- src/concerns/API.js
+++ src/concerns/API.js
@@ -1,7 +1,7 @@
 const getBaseEndpoint = () => {
   return process.env.NODE_ENV === "development"
     ? "http://localhost:3000/"
-    : "https://hueman-server-eu.herokuapp.com"
+    : "https://api.playhueman.com"
 }
 
 const BASE_URL = getBaseEndpoint()
